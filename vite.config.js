6c7b9cd073301486558fc27e6f523fdb27585239
Lauren S. Keiper <lk565747@loras.edu>
2025-11-15 17:57:19 -0600

    First Fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-react-site/',
+  base: '/Lauren-Website/',
   plugins: [react()]
 })
